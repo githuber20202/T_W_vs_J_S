390b51bfd0134327bec10174be603608513d7a7b
githuber20202 <59474973+githuber20202@users.noreply.github.com>
2022-12-08 22:43:58 +0200

    111

--- CollectLogs.java
+++ CollectLogs.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+import java.util.stream.*;
+
 public class CollectLogs {
     public boolean ValidateLog1() {
         boolean result = true;
+        System.out.println("1 st Text");
         return result;
     }
 
